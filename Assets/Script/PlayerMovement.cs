5f976cef4a6d30a05f3bbc3552a80ca12562fa42
Emirhan Turan <emirhanturanfb@hotmail.com>
2024-02-26 17:59:39 +0300

    PlayerMovement Scirpt Update

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,7 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
 
    private Rigidbody rb;
-    [SerializeField] private float speed = 0;
+   [SerializeField] private float forward_Speed = 0;
+   [SerializeField] private float Direction_Speed = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,13 @@ public class PlayerMovement : MonoBehaviour
    
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(-speed*Time.deltaTime,rb.velocity.y,rb.velocity.z);
+        rb.velocity = new Vector3(-forward_Speed * Time.deltaTime,rb.velocity.y,rb.velocity.z);
+
+        if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, Input.GetAxis("Horizontal") * Direction_Speed * Time.deltaTime);
+        }
     }
+
+
 }
